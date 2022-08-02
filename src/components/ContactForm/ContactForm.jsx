ff6698e5ac744e2312dd982c925fd7c1978b9eab
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-08-02 09:09:53 +0300

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -29,6 +29,7 @@ class ContactForm extends Component {
           className={s.input}
           type="text"
           name="name"
+          value={this.state.name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -41,6 +42,7 @@ class ContactForm extends Component {
           className={s.input}
           type="tel"
           name="number"
+          value={this.state.number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
